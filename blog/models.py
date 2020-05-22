b940b5224600ece98671c103924a0616f02e4c1b
nm01n <nemo0mb@gmail.com>
2020-05-22 10:07:25 +0300

    urls added

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.urls import reverse
 
 class Post(models.Model):
     STATUS_CHOICES = (
@@ -21,6 +22,12 @@ class Post(models.Model):
                               choices=STATUS_CHOICES,
                               default='draft')
     objects = models.Manager() # The default manager.
+    def get_absolute_url(self):
+        return reverse('blog:post_detail',
+                       args=[self.publish.year,
+                             self.publish.month,
+                             self.publish.day,
+                             self.slug])
     
 class Meta:
     ordering = ('-publish',)
