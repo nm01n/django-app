c802c4f3c916af26cd62d9b2c4f79b279127266e
nm01n <nemo0mb@gmail.com>
2020-05-22 18:36:51 +0300

    sharing by e-mail added

--- blog/models.py
+++ blog/models.py
@@ -3,6 +3,10 @@ from django.utils import timezone
 from django.contrib.auth.models import User
 from django.urls import reverse
 
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset().filter(status='published')
+
 class Post(models.Model):
     STATUS_CHOICES = (
         ('draft', 'Draft'),
@@ -21,23 +25,18 @@ class Post(models.Model):
     status = models.CharField(max_length=10,
                               choices=STATUS_CHOICES,
                               default='draft')
+
     objects = models.Manager() # The default manager.
+    published = PublishedManager() # Our custom manager.
+
+    class Meta:
+        ordering = ('-publish',)
+
+    def __str__(self):
+        return self.title
+
     def get_absolute_url(self):
         return reverse('blog:post_detail',
                        args=[self.publish.year,
                              self.publish.month,
-                             self.publish.day,
-                             self.slug])
-    
-class Meta:
-    ordering = ('-publish',)
-
-def __str__(self):
-    return self.title
-
-class PublishedManager(models.Manager):
-    def get_queryset(self):
-        return super(PublishedManager,
-                     self).get_queryset()\
-                          .filter(status='published')
-
+                             self.publish.day, self.slug])
--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,54 @@
 from django.shortcuts import render, get_object_or_404
+from django.core.paginator import Paginator, EmptyPage,\
+                                  PageNotAnInteger
+from django.views.generic import ListView
 from .models import Post
+from .forms import EmailPostForm
+from django.core.mail import send_mail
+
+def post_share(request, post_id):
+    # Retrieve post by id
+    post = get_object_or_404(Post, id=post_id, status='published')
+    sent = False
+
+    if request.method == 'POST':
+        # Form was submitted
+        form = EmailPostForm(request.POST)
+        if form.is_valid():
+            # Form fields passed validation        
+            cd = form.cleaned_data
+            # ... send email
+            post_url = request.build_absolute_url(post.get_absolute_url())
+            subject = f"{cd['name']} recommends you read " \
+                      f"{post.title}"
+            message = f"Read {post.title} at {post_url}\n\n" \
+                      f"{cd['name']}\'s comments: {cd['comments']}"
+            send_mail(subject, message, 'nemo0mb@gmail.com', [cd['to']])
+            sent = True
+
+    else:
+        form = EmailPostForm()
+    return render(request, 'blog/post/share.html', {'post': post,
+                                                        'form': form,
+                                                        'sent': sent})
 
 def post_list(request):
-    posts = Post.published.all()
+    object_list = Post.published.all()
+    paginator = Paginator(object_list, 3) # 3 posts in each page
+    page = request.GET.get('page')
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        # If page is not an integer deliver the first page
+        posts = paginator.page(1)
+    except EmptyPage:
+        # If page is out of range deliver last page of results
+        posts = paginator.page(paginator.num_pages)
     return render(request,
                  'blog/post/list.html',
-                 {'posts': posts})
+                 {'page': page,
+                  'posts': posts})
+
 
 def post_detail(request, year, month, day, post):
     post = get_object_or_404(Post, slug=post,
@@ -15,4 +58,11 @@ def post_detail(request, year, month, day, post):
                                    publish__day=day)
     return render(request,
                   'blog/post/detail.html',
-                  {'post': post})                           
+                  {'post': post})
+
+
+class PostListView(ListView):
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = 'blog/post/list.html'
