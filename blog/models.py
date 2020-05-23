a2c0d9356928e3c8241fda9d6105e9a50fe45be8
nm01n <nemo0mb@gmail.com>
2020-05-23 05:55:45 +0300

    sharing by e-mail deleted

--- blog/models.py
+++ blog/models.py
@@ -47,8 +47,7 @@ class Comment(models.Model):
     post = models.ForeignKey(Post,
                              on_delete=models.CASCADE,
                              related_name='comments')
-    name = models.CharField(max_length=80)
-    email = models.EmailField()
+    name = models.CharField(max_length=80)   
     body = models.TextField()
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
--- blog/views.py
+++ blog/views.py
@@ -2,15 +2,14 @@ from django.db.models import Count
 from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator, EmptyPage,\
                                   PageNotAnInteger
-from django.core.mail import send_mail
 from django.views.generic import ListView
 from .models import Post, Comment
-from .forms import EmailPostForm, CommentForm
+from .forms import CommentForm
 
 def post_list(request):
     object_list = Post.published.all()
 
-    paginator = Paginator(object_list, 3) # 100 posts in each page
+    paginator = Paginator(object_list, 100) # 100 posts in each page
     page = request.GET.get('page')
     try:
         posts = paginator.page(page)
@@ -70,30 +69,8 @@ def post_detail(request, year, month, day, post):
 class PostListView(ListView):
     queryset = Post.published.all()
     context_object_name = 'posts'
-    paginate_by = 3
+    paginate_by = 100
     template_name = 'blog/post/list.html'
 
 
-def post_share(request, post_id):
-    # Retrieve post by id
-    post = get_object_or_404(Post, id=post_id, status='published')
-    sent = False
-
-    if request.method == 'POST':
-        # Form was submitted
-        form = EmailPostForm(request.POST)
-        if form.is_valid():
-            # Form fields passed validation
-            cd = form.cleaned_data
-            post_url = request.build_absolute_uri(post.get_absolute_url())
-            subject = f"{cd['name']} recommends you read {post.title}"
-            message = f"Read {post.title} at {post_url}\n\n" \
-                      f"{cd['name']}\'s comments: {cd['comments']}"
-            send_mail(subject, message, 'admin@myblog.com', [cd['to']])
-            sent = True
-
-    else:
-        form = EmailPostForm()
-    return render(request, 'blog/post/share.html', {'post': post,
-                                                    'form': form,
-                                                    'sent': sent})
+   
