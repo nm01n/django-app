cbe6d045083d4102ccfc6e092adc4c640de4e4c5
nm01n <nemo0mb@gmail.com>
2020-05-23 06:33:47 +0300

    pagination deleted

--- blog/views.py
+++ blog/views.py
@@ -1,28 +1,14 @@
 from django.db.models import Count
 from django.shortcuts import render, get_object_or_404
-from django.core.paginator import Paginator, EmptyPage,\
-                                  PageNotAnInteger
 from django.views.generic import ListView
 from .models import Post, Comment
 from .forms import CommentForm
 
-def post_list(request):
-    object_list = Post.published.all()
 
-    paginator = Paginator(object_list, 100) # 100 posts in each page
-    page = request.GET.get('page')
-    try:
-        posts = paginator.page(page)
-    except PageNotAnInteger:
-        # If page is not an integer deliver the first page
-        posts = paginator.page(1)
-    except EmptyPage:
-        # If page is out of range deliver last page of results
-        posts = paginator.page(paginator.num_pages)
+def post_list(request):    
     return render(request,
                  'blog/post/list.html',
-                 {'page': page,
-                  'posts': posts})
+                  {'posts': posts})
 
 
 def post_detail(request, year, month, day, post):
